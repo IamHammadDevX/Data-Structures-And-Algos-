c0cdf68a3c4582e3bb8d543a85aa2d1def5762bd
HAMMAD <hammadehsan3@gmail.com>
2024-07-31 02:39:37 +0500

    Reverse string logic updated

--- CharacterArraysAndStrings/ReverseStr.cpp
+++ CharacterArraysAndStrings/ReverseStr.cpp
@@ -1,25 +1,54 @@
-// #include<iostream>
-// using namespace std;
+// C++ program for the above approach
 
-// // Reverse the string
-// void revStr(string str) {
-//     int s = 0;
-//     int e = str.length() - 1;
+#include <bits/stdc++.h>
+using namespace std;
 
-//     while (s <= e)
-//     {
-//         swap(str[s++], str[e--]);
+// Function to reverse the given string
+string reverseString(string str)
+{
 
-//     }
+	// Reverse str using inbuilt function
+	reverse(str.begin(), str.end());
 
-// }
+	// Add space at the end so that the
+	// last word is also reversed
+	str.insert(str.end(), ' ');
 
+	int n = str.length();
 
-// int main() {
+	int j = 0;
 
-//     string str = "I am the man";
-//     revStr(str);
-//     cout << "Your String: " << str << endl;
+	// Find spaces and reverse all words
+	// before that
+	for (int i = 0; i < n; i++) {
 
-//     return 0;
-// }
+		// If a space is encountered
+		if (str[i] == ' ') {
+			reverse(str.begin() + j, str.begin() + i);
+
+			// Update the starting index
+			// for next word to reverse
+			j = i + 1;
+		}
+	}
+
+	// Remove spaces from the end of the
+	// word that we appended
+	str.pop_back();
+
+	// Return the reversed string
+	return str;
+}
+
+// Driver code
+int main()
+{
+	string str = "I like this code";
+
+	// Function call
+	string rev = reverseString(str);
+
+	// Print the reversed string
+	cout << rev;
+	return 0;
+}
